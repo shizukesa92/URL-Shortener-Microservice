c28533bbbfd61713b2a106f22343308b0ef16eea
Leonard Tye <leonardtye1992@gmail.com>
2018-04-08 10:13:51 +0800

    MongoDB settings

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const api = require("./server/routes/api");
+const dotenv = require("dotenv");
+const url = process.env.MONGOLAB_URI;
 
+
+dotenv.config(); // Good for development only
 app.use("/api/", api);
 app.use(express.static("./dist/client"));
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname + "./dist/client/index.html"));
 });
 app.listen(process.env.PORT || 3000);
+
+//heroku config:set MONGOLAB_URI=mongodb://<dbuser>:<dbpassword>@ds237989.mlab.com:37989/url-shortener-microservice
